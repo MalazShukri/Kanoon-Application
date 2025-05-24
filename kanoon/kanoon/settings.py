3bc845a69a30f0e86042ee17c852698cdfcc927e
Malaz Shukri <abuda.b80.ab@gmail.com>
2025-05-24 22:39:36 +0300

    static files

--- kanoon/kanoon/settings.py
+++ kanoon/kanoon/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 from decouple import config, Csv
-from datetime import tim
+from datetime import time
 
 
 SECRET_KEY = config('SECRET_KEY')
@@ -117,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 AUTH_USER_MODEL = 'main.User'
 
